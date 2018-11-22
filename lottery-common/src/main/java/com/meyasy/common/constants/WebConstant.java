6f2e0bc9567dfa48dcd3112ee0f7d45aa1bba6f4
chengzilong <237643381@qq.com>
2018-11-22 09:59:05 +0800

    调整编译文件

--- lottery-common/src/main/java/com/meyasy/common/constants/WebConstant.java
+++ lottery-common/src/main/java/com/meyasy/common/constants/WebConstant.java
@@ -1,7 +1,7 @@
 package com.meyasy.common.constants;
 
 public interface WebConstant {
-     String SSO_ACCESS_URL="/pages/login.html";
+     String SSO_ACCESS_URL="/pages/index.html";
      String ACTIVITY_ACCESS_URL="/pages/activity.html";
 
 }
